2b7ee16e7ed233ca7b752f807122135c983b2677
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-30 15:52:53 +0200

    [UPD] bonus 2

--- js/main.js
+++ js/main.js
@@ -5,18 +5,32 @@ const ul = document.querySelector("ul.list");
 for (let i = 1; i <= 100; i++) {
 
     const li = document.createElement("li");
-    
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        li.append("FizzBuzz");
+        span.append("FizzBuzz");
+        div.append(span);
+        div.classList.add("square", "red");
+        li.append(div);
         ul.append(li);
     } else if (i % 3 == 0) {
-        li.append("Fizz");
+        span.append("Fizz");
+        div.append(span);
+        div.classList.add("square", "green");
+        li.append(div);
         ul.append(li);
     } else if (i % 5 == 0) {
-        li.append("Buzz");
+        span.append("Buzz");
+        div.append(span);
+        div.classList.add("square", "yellow");
+        li.append(div);
         ul.append(li);
     } else {
-        li.append(i);
+        span.append(i);
+        div.append(span);
+        div.classList.add("square", "blue");
+        li.append(div);
         ul.append(li);
     }
 }
